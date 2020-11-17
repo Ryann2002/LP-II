2fd5f97b4db4a8dd52f88952a2c353c87f9246fa
Ryan Ferreira <ryanferreira135@gmail.com>
2020-11-17 11:13:14 -0300

    Prova

---
+++ Prova/P0030482011038/P0030482011038/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace P0030482011038
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lbxResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(90, 125);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(132, 133);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lbxResults
+            // 
+            this.lbxResults.FormattingEnabled = true;
+            this.lbxResults.Location = new System.Drawing.Point(387, 36);
+            this.lbxResults.Name = "lbxResults";
+            this.lbxResults.Size = new System.Drawing.Size(373, 368);
+            this.lbxResults.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbxResults);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox lbxResults;
+    }
+}
+
---
+++ Prova/P0030482011038/P0030482011038/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030482011038
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            lbxResults.Items.Clear();
+            //Final RA = 8
+            double[,] meses = new double[8, 4];
+            string valorSemana;
+            double totMes = 0;
+            double totTotal = 0;
+            
+
+            for (int mes = 0; mes < 8; mes++)
+            {
+
+                for (int semana = 0; semana < 4; semana++)
+                {
+                    valorSemana = Interaction.InputBox("Insira o valor recebido na semana " + (semana + 1)  + " do mes " + (mes + 1), "Digite os valores", "0");
+                    double valor;
+                    try {
+                        valor = Double.Parse(valorSemana);
+                        meses[mes, semana] = valor;
+                        totMes += valor;
+                        totTotal += valor;
+
+                    } catch {
+
+                        MessageBox.Show("Valor Inválido");
+                        valor = 0;
+                    }
+
+                    lbxResults.BeginUpdate();
+
+                    lbxResults.Items.Add("Total do mes: " + (mes + 1) + " semana " + (semana + 1) + " = " + String.Format("{0:C2}", valor));
+
+                    lbxResults.EndUpdate();
+                }
+
+                lbxResults.BeginUpdate();
+
+                lbxResults.Items.Add(">> Total do mês: "  + String.Format("{0:C2}", totMes));
+                lbxResults.Items.Add("-----------------------------------");
+                lbxResults.EndUpdate();
+                totMes = 0;
+
+            }
+
+
+
+
+            lbxResults.BeginUpdate();
+
+            lbxResults.Items.Add(">> Total Geral: " + String.Format("{0:C2}", totTotal));
+            lbxResults.Items.Add("-----------------------------------");
+            lbxResults.EndUpdate();
+            totTotal = 0;
+
+
+        }
+    }
+}
